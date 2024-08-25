382f1f4c847cbc533f1fecf585f0b2ce505a0c83
mango_buff <208079635@qq.com>
2024-08-25 16:28:13 +0800

    Project Move

---
+++ src/main/java/com/mango_buff/botany_pots_ore_planting/BotanyPotsOrePlanting.java
@@ -0,0 +1,9 @@
+package com.mango_buff.botany_pots_ore_planting;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(BotanyPotsOrePlanting.MODID)
+public class BotanyPotsOrePlanting {
+    public static final String MODID = "botany_pots_ore_planting";
+}
