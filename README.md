99a11fa12a7827fa9bce8267e7162e843c573add
mango_buff <208079635@qq.com>
2025-08-08 15:42:41 +0800

    Support for Minecraft 1.21.1.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# This is the branch of MC-1.20.1
+# This is the branch of MC-1.21.1
 **For detailed information, please refer to the README.md in the main branch**
---
+++ neoforge/src/main/java/com/mango_buff/botany_pots_ore_planting/BotanyPotsOrePlanting.java
@@ -0,0 +1,8 @@
+package com.mango_buff.botany_pots_ore_planting;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(BotanyPotsOrePlanting.MODID)
+public class BotanyPotsOrePlanting {
+    public static final String MODID = "botany_pots_ore_planting";
+}
